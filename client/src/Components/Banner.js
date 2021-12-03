e47a67839e812ebf273c7bedbf463b930832888e
cambateman <cambateman604@gmail.com>
2021-12-03 15:10:07 -0800

    added Banner tagline and anmiations

---
+++ client/src/Components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <h1 className="banner-header">Full-Stack Web Developer</h1>
+            <p className="banner-paragraph">
+                <i>Hello! I'm Cam Bateman, a Full-stack Web Developer who can't wait to                        
+                   help you create a Website or Web Application!</i>
+            </p>
+        </div>
+    )
+}
+
+export default Banner
