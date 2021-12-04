5f1d2e635b7df63b57a5baf654a1f7bdbf84098b
cambateman <cambateman604@gmail.com>
2021-12-03 16:31:20 -0800

    added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,14 @@ import SkillCard from "./Components/SkillCards";
 import frontEnd from './assets/front-end.png'
 import backEnd from './assets/back-end.png'
 import databases from './assets/databases.png'
+import Footer from "./Components/Footer";
 
 
 function App() {
   return (
-    <div className="index-page-cards">
-      <SkillCard
-            img={frontEnd}
-            title="Front-end Development"
-            information="Responsive Websites for your Event, Organziation, Small bussiness,
-            and everything in between! I am able to provide the site, hosting                        
-            responsibilities, and any other updates you need!"
-      />
-
-      <SkillCard
-            img={backEnd}
-            title="Back-end Development"
-            information="For those that need their website to 'do something' like: Allow users to log in and out, E-commerce functionality to sell your specialty products, Set-up mailing lists, Keep track of Appointments, or show an interactive schedule"
-      />
-
-      <SkillCard
-            img={databases}
-            title="Database Administration "
-            information="Managment or creation of SQL or NoSQL to manage your customers, orders, inventory, and provide reports"
-      />
+    <div className="App">
+      <Footer />
+      
     </div>
   );
 }
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const githubIcon = <FontAwesomeIcon icon={faGithub} />
+const twitterIcon = <FontAwesomeIcon icon={faTwitter} />
+const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} />
+const instagramIcon = <FontAwesomeIcon icon={faInstagram} />
+
+
+function Footer() {
+    return (
+        <footer>
+            <meta charset="UTF-8"/>
+            <h2 className="footer-h2">Cam Bateman {new Date().getFullYear()}&copy;</h2>
+            
+            <ul className="footer-links">
+                <a className="footer-a" href="https://github.com/cambateman"><li className="footer-li-a">{githubIcon}  Github</li></a>
+                <a className="footer-a" href="https://www.linkedin.com/in/cam-bateman/"><li className="footer-li-a">{linkedinIcon}  LinkedIn</li></a>
+                <li className="footer-li-a">{twitterIcon}  Twitter</li>
+                <li className="footer-li-a">{instagramIcon}  Instagram</li>
+            </ul>
+        </footer>
+        )
+}
+
+export default Footer
