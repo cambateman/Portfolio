9e45f29cbcf1e4183170230b492e8dd835d74e5d
cambateman <cambateman604@gmail.com>
2021-12-14 09:43:20 -0800

    code reformat

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -10,19 +10,23 @@ function About() {
                 </div>
                 <div className="about-p-container">
                     <p className="about-p">Welcome to my Portfolio!</p>
-                    <p className="about-p">I'm a Fullstack Web Developer from Vancouver BC. I took a 
-                                            coding course in University before graduating. After getting some work 
-                                            expereience with one of Vancouver's premiere employers, STEMCELL Technologies,
-                                            I decieded to invest in my technical skillset by learning how to code.
+                    <p className="about-p">
+                        I'm a Fullstack Web Developer from Vancouver BC. I took a 
+                        coding course in University before graduating. After getting some work 
+                        expereience with one of Vancouver's premiere employers, STEMCELL Technologies,
+                        I decided to invest in my technical skillset by learning how to code.
                     </p>
-                    <p className="about-p">I am a graduate of the Software Systems Developer program at BCIT. The Software Systems Developer program features an accelerated, 
-                                            project-based learning environment to prepare students for a career as a full-stack web developer. The 8-month program provides a 
-                                            strong foundation in multiple emerging web and mobile application development tools and technologies. It is completed with a 4 week
-                                            Industry Project working hands on building Production Application as needed by the Host                                            
+                    <p className="about-p">
+                        I am a graduate of the Software Systems Developer program at BCIT. The Software Systems Developer program features an accelerated, 
+                        project-based learning environment to prepare students for a career as a full-stack web developer. The 8-month program provides a 
+                        strong foundation in multiple emerging web and mobile application development tools and technologies. It is completed with a 4 week
+                        Industry Project working hands on building Production Application as needed by the Host                                            
                     </p>
 
-                    <p className="about-p">Outside of coding, I love sports and reading. I enjoy staying in shape and have a sport for every almost every season. Golfing in the summer, 
-                                            lacrosse in the fall, and skiing in the winter. I also enjoy trying my hand in the kitchen whenever i find the inspiration.
+                    <p className="about-p">
+                        
+                        Outside of coding, I love sports and reading. I enjoy staying in shape and have a sport for every almost every season. Golfing in the summer, 
+                        lacrosse in the fall, and skiing in the winter. I also enjoy trying my hand in the kitchen whenever i find the inspiration.
                     </p>
                 </div>
             </div>
